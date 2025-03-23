1693d0dfdc3b55660b541cb5649a0a3365526c8b
sanchiitvijay <sanchiitvijay@gmail.com>
2025-03-23 08:43:07 +0530

    fix some bug

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -3,8 +3,8 @@ const Event = require('../models/event.models');
 
 exports.createTask = async (req, res) => {
     try {
-        const { name, startTime, endTime, maxVolunteerCount, maxVolunteerNeeded } = req.body;
-        if (!name || !startTime || !endTime || !maxVolunteerCount || !maxVolunteerNeeded) {
+        const { name, startTime, endTime, currentVolunteerCount, maxVolunteerNeeded } = req.body;
+        if (!name || !startTime || !endTime || !currentVolunteerCount || !maxVolunteerNeeded) {
             return res.status(400).json({
                 success: false,
                 message: "All fields are required"
@@ -15,7 +15,7 @@ exports.createTask = async (req, res) => {
             name,
             startTime,
             endTime,
-            maxVolunteerCount,
+            currentVolunteerCount,
             maxVolunteerNeeded
         });
     
@@ -79,6 +79,50 @@ exports.deleteTask = async (req, res) => {
     }
     catch (error) {
         console.log("Error occured while deleting task", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
+
+exports.updateTask = async (req, res) => {
+    try {
+        const { taskId, name, startTime, endTime, currentVolunteerCount, maxVolunteerNeeded } = req.body;
+        
+        if (!taskId || !name || !startTime || !endTime || !currentVolunteerCount || !maxVolunteerNeeded) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        const task = await Task.findById(taskId);
+
+        if (!task) {
+            return res.status(404).json({
+                success: false,
+                message: "Task not found"
+            })
+        }
+
+        task.name = name;
+        task.startTime = startTime;
+        task.endTime = endTime;
+        task.currentVolunteerCount = currentVolunteerCount;
+        task.maxVolunteerNeeded = maxVolunteerNeeded;
+
+        await task.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Task updated successfully",
+            data: task
+        })
+    }
+    catch (error) {
+        console.log("Error occured while updating task", error);
         return res.status(500).json({
             success: false,
             message: "Internal server error"
